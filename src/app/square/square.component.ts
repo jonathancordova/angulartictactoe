25d9773d14a9b430411dc6c298d934c441de827d
Jonathan Cordova <jonathandavidcordova@gmail.com>
2020-08-09 15:36:52 -0400

    added board

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,11 +1,14 @@
-import { Component} from '@angular/core';
+import { Component,Input} from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  templateUrl: './square.component.html',
-  styleUrls: ['./square.component.scss']
+  templateUrl: `
+  <button>{{value}}</button>
+  `,
+  styleUrls: []
 })
 export class SquareComponent{
+  @Input() value : 'X' | 'O';
 
   constructor() { }
 
